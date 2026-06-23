2b490d41b7f959fe3053f068cbe17fc7cca2408c
sushumanth <tsushmanth@gmail.com>
2026-06-23 18:53:45 +0530

    Add dynamic import error catcher wrapper to api/boot.ts

--- api/boot.ts
+++ api/boot.ts
@@ -1,2 +1,26 @@
-import app from "../server/boot";
+import express from "express";
+
+let app: any;
+let bootError: any = null;
+
+try {
+  const boot = await import("../server/boot");
+  app = boot.default;
+} catch (err: any) {
+  bootError = err;
+  console.error(">>> Vercel Serverless Function Boot Error:", err);
+}
+
+if (bootError) {
+  const mockApp = express();
+  mockApp.use((req, res) => {
+    res.status(500).json({
+      error: "Vercel Boot Error",
+      message: bootError.message || String(bootError),
+      stack: bootError.stack || "",
+    });
+  });
+  app = mockApp;
+}
+
 export default app;
